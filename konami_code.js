785d9375feaa6e15d31105f7850ec145fd993807
Ian Pollack <ianjpollack@gmail.com>
2018-03-08 21:37:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,4 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  document.body.addEventListener('keydown', function youGotIt(code) {
-    if (code) {
-      alert("Congraulations!")
-    } else {
-      
-  })
-
-
-
-
-
-
-
-
-
-  let index = 0;
-  function youGotIt(e) {
-    const key = parseInt(e.detail || e.which);
-    if (key === )
-  }
-
 }

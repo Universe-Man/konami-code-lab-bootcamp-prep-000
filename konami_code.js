8021563007ece66f0232c199dee3cdfe465e904f
Ian Pollack <ianjpollack@gmail.com>
2018-03-08 21:31:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
-const world = document.body;
-
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function youGotIt(code) {
+    
+  })
+
+
+
+
+
+
+
+
+
+  let index = 0;
+  function youGotIt(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === )
+  }
 
 }
-
-
-const input = document.querySelector('input')
-
-input.addEventListener('keydown', function(e) {
-  console.log(e.which)
-})
